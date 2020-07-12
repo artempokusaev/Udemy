e7c07304dcb0526d872a1acfb2e3603faadb324a
aa.pokusaev <artempokusaev@gmail.com>
2020-07-12 13:19:01 +0300

    Добавил файл chapter2

--- chapter3.py
+++ chapter3.py
@@ -1,9 +1,15 @@
-import datetime
-mynow = datetime.datetime.now()
-print(mynow)
+# import datetime
+# mynow = datetime.datetime.now()
+# print(mynow)
 
-x = 10
-y = "10"
-z = 10.1
+# x = 10
+# y = "10"
+# z = 10.1
 
-print(type(x), type(y), type(z))
+# print(type(x), type(y), type(z))
+
+student_grades = [9.1, 8.8, 7.5]
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
