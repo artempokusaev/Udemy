6f17c0099eea94413b950f17516284b00231a6f4
aa.pokusaev <artempokusaev@gmail.com>
2020-07-17 14:28:08 +0300

    Функции

---
+++ chapter5.py
@@ -0,0 +1,14 @@
+#Functions
+
+def mean (value):
+    if type(value) == dict:
+        the_mean = sum(value.values()) / len(value)
+    else:
+        the_mean = sum(value) / len(value)
+    #return the_mean
+    return the_mean
+
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+
+
+print(mean(student_grades))
