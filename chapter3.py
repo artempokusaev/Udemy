4b4f3064624ee393951de5dd068a05ebf9b7faa7
aa.pokusaev <artempokusaev@gmail.com>
2020-07-17 11:49:28 +0300

    Закоммитил домашние исправления на работе

--- chapter3.py
+++ chapter3.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # import datetime
 # mynow = datetime.datetime.now()
 # print(mynow)
@@ -13,3 +14,7 @@ mysum = sum(student_grades)
 length = len(student_grades)
 mean = mysum / length
 print(mean)
+=======
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+print(student_grades)
+>>>>>>> b97286fdf7996ceec8006f63e8c866029bb43b1f
