5aa18ca5de37683aec2a4381720f69488ad762a6
aa.pokusaev <artempokusaev@gmail.com>
2020-07-31 17:02:23 +0300

    Добавил файлы для chapter 8, 9

---
+++ chapter9.py
@@ -0,0 +1,9 @@
+def check_numbers(user_input):
+    only_numbers = [numbers for numbers in user_input if not isinstance(user_input, str)]
+    #return only_numbers
+
+
+while True:
+
+
+print(check_numbers(input()))
